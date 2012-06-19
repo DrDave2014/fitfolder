f9f0cd395fb0fa1f0c2a382bbabce379ad959461
Dave Scheuerman <dave33@thescheuermans.com>
2012-06-19 12:00:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitfolder::Application.config.secret_token = '3fca3d6292a765bcec1fafce2a5dd0926bdc7221f3a50cadb1a4fc52cd6edb1a60ffddf072aff6f1412c3bb7590232b6068dbe74d2dce53e4ed122965272677b'
